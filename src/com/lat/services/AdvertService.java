2ffdb2393d47885954ba4c320ea1a1d599f5bd45
Maxime Signoret <maxime.signoret@gmail.com>
2015-12-07 16:43:50 +0100

    [maxime/advert-service] Move advert logic into a service

--- src/com/lat/services/AdvertService.java
+++ src/com/lat/services/AdvertService.java
@@ -1,19 +1,57 @@
 package com.lat.services;
 
+import com.lat.beans.Advert;
+import com.lat.dao.AdvertDao;
+import com.lat.dao.DAOFactory;
+import com.lat.dao.UserDao;
+import com.lat.forms.AdvertAddForm;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.List;
+
 public class AdvertService
 {
-    private static AdvertService SESSION = null;
+    private static AdvertService ADVERT_SERVICE = null;
+    private HttpSession session;
+    private UserDao userDao;
+    private AdvertDao advertDao;
+    private AdvertAddForm advertAddForm;
 
     private AdvertService()
     {
+        this.userDao = DAOFactory.getInstance().getUserDao();
+        this.advertDao = DAOFactory.getInstance().getAdvertDao();
+        this.advertAddForm = new AdvertAddForm(this.advertDao);
     }
 
     public static AdvertService getInstance()
     {
-        if (SESSION == null)
+        if (ADVERT_SERVICE == null)
         {
-            SESSION = new AdvertService();
+            ADVERT_SERVICE = new AdvertService();
         }
-        return SESSION;
+
+        return ADVERT_SERVICE;
+    }
+
+    public List<Advert> getAllAdverts()
+    {
+        return this.advertDao.find();
+    }
+
+    public AdvertAddForm getAdvertForm()
+    {
+        return this.advertAddForm;
+    }
+
+    public Advert processAdvert(HttpServletRequest request)
+    {
+        return this.advertAddForm.processAdvert(request);
+    }
+
+    public Advert getAdvert(int id)
+    {
+        return this.advertDao.findOneById(id);
     }
 }
--- src/com/lat/servlets/AddAdvert.java
+++ src/com/lat/servlets/AddAdvert.java
@@ -1,9 +1,6 @@
 package com.lat.servlets;
 
-import com.lat.beans.Advert;
-import com.lat.dao.AdvertDao;
-import com.lat.dao.DAOFactory;
-import com.lat.forms.AdvertAddForm;
+import com.lat.services.AdvertService;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -13,36 +10,23 @@ import java.io.IOException;
 
 public class AddAdvert extends HttpServlet
 {
-    public static final String CONF_DAO_FACTORY = "daofactory";
-    public static final String ATT_ADVERT = "advert";
-    public static final String ATT_FORM = "form";
-    public static final String VUE = "/WEB-INF/addAdvert.jsp";
+    private AdvertService advertService;
 
-    private AdvertDao advertDao;
-
-    public void init() throws ServletException {
-        /* Récupération d'une instance de notre DAO Advert */
-        this.advertDao = ((DAOFactory) getServletContext().getAttribute(CONF_DAO_FACTORY)).getAdvertDao();
+    public void init() throws ServletException
+    {
+        this.advertService = AdvertService.getInstance();
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
-        /* Affichage de la page de création d'une offre de prêt */
-        this.getServletContext().getRequestDispatcher(VUE).forward(request, response);
+        this.getServletContext().getRequestDispatcher("/WEB-INF/addAdvert.jsp").forward(request, response);
     }
 
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
-        /* Préparation de l'objet formulaire */
-        AdvertAddForm form = new AdvertAddForm(advertDao);
+        request.setAttribute("form", this.advertService.getAdvertForm());
+        request.setAttribute("advert", this.advertService.processAdvert(request));
 
-        /* Appel au traitement et à la validation de la requête, et récupération du bean en résultant */
-        Advert advert = form.processAdvert(request);
-
-        /* Stockage du formulaire et du bean dans l'objet request */
-        request.setAttribute(ATT_FORM, form);
-        request.setAttribute(ATT_ADVERT, advert);
-
-        this.getServletContext().getRequestDispatcher(VUE).forward(request, response);
+        this.getServletContext().getRequestDispatcher("/WEB-INF/addAdvert.jsp").forward(request, response);
     }
 }
