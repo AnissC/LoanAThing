ff92d70a7e29faf88cf0ec7e9e53af56e7183d6e
Nico Plop <nyckoo@live.fr>
2015-12-23 15:53:21 +0100

    [lot-of-design] Fixed scroll design on home page and MOCK data for adverts

---
+++ web/WEB-INF/lib/advertMOCK.sql
@@ -0,0 +1,233 @@
+INSERT INTO `advert` (
+  `id`,
+  `title`,
+  `description`,
+  `short_description`,
+  `image`,
+  `date_start`,
+  `date_end`,
+  `user_id`,
+  `category_id`,
+  `is_suspend`,
+  `is_publish`
+) VALUES (
+  1,
+  'Chaussure BlackStone',
+  'Chaussure cuir montante de marque Blackstone fabriquée a la main  et fourées en veritable laine de mouton. Pointure 44.',
+  NULL,
+  'chaussure-blackstone.png',
+  now(),
+  '2016-01-02',
+  '2',
+  '5',
+  0,
+  1
+);
+
+INSERT INTO `advert` (
+  `id`,
+  `title`,
+  `description`,
+  `short_description`,
+  `image`,
+  `date_start`,
+  `date_end`,
+  `user_id`,
+  `category_id`,
+  `is_suspend`,
+  `is_publish`
+) VALUES (
+  2,
+  'T-Shirt Star Wars',
+  'T-Shirt Star Wars pour les plus grands fans qui souhaitent se rendre au cinema vetu d un T Shirt au couleur de leur films préféré !',
+  NULL,
+  'tshirt-starwars.png',
+  now(),
+  '2016-01-03',
+  '2',
+  '7',
+  0,
+  1
+);
+
+INSERT INTO `advert` (
+  `id`,
+  `title`,
+  `description`,
+  `short_description`,
+  `image`,
+  `date_start`,
+  `date_end`,
+  `user_id`,
+  `category_id`,
+  `is_suspend`,
+  `is_publish`
+) VALUES (
+  3,
+  'BB-8',
+  'Un petit robot rond telecommandé qui parcourera votre salon de son allure non chalante pour ravir petits et grands',
+  NULL,
+  'bb8.png',
+  now(),
+  '2016-01-03',
+  '2',
+  '1',
+  0,
+  1
+);
+
+INSERT INTO `advert` (
+  `id`,
+  `title`,
+  `description`,
+  `short_description`,
+  `image`,
+  `date_start`,
+  `date_end`,
+  `user_id`,
+  `category_id`,
+  `is_suspend`,
+  `is_publish`
+) VALUES (
+  9,
+  'BB-8',
+  'Un petit robot rond telecommandé qui parcourera votre salon de son allure non chalante pour ravir petits et grands',
+  NULL,
+  'bb8.png',
+  now(),
+  '2016-01-03',
+  '2',
+  '1',
+  0,
+  1
+);
+
+INSERT INTO `advert` (
+  `id`,
+  `title`,
+  `description`,
+  `short_description`,
+  `image`,
+  `date_start`,
+  `date_end`,
+  `user_id`,
+  `category_id`,
+  `is_suspend`,
+  `is_publish`
+) VALUES (
+  4,
+  'Moniteur',
+  'Un écran 22.5 pouces pour les informaticiens en cruel manque d ecran, car on a jamais trop d ecrans !',
+  NULL,
+  'ecran.png',
+  now(),
+  '2016-01-03',
+  '2',
+  '1',
+  0,
+  1
+);
+
+INSERT INTO `advert` (
+  `id`,
+  `title`,
+  `description`,
+  `short_description`,
+  `image`,
+  `date_start`,
+  `date_end`,
+  `user_id`,
+  `category_id`,
+  `is_suspend`,
+  `is_publish`
+) VALUES (
+  5,
+  'Machine à pop Corn',
+  'Une machine pour faire des pop corn ! Cette machine va transformer vos soirée Netflix en véritables seances de cinéma',
+  NULL,
+  'popcorn.png',
+  now(),
+  '2016-01-03',
+  '2',
+  '2',
+  0,
+  1
+);
+
+INSERT INTO `advert` (
+  `id`,
+  `title`,
+  `description`,
+  `short_description`,
+  `image`,
+  `date_start`,
+  `date_end`,
+  `user_id`,
+  `category_id`,
+  `is_suspend`,
+  `is_publish`
+) VALUES (
+  6,
+  'Maillot de Foot',
+  'Un superbe maillot de foot pour vous permettre d aller faire semblant de s etre blesser sur un terrain de foot entre pote !',
+  NULL,
+  'maillotfoot.png',
+  now(),
+  '2016-01-03',
+  '2',
+  '3',
+  0,
+  1
+);
+
+INSERT INTO `advert` (
+  `id`,
+  `title`,
+  `description`,
+  `short_description`,
+  `image`,
+  `date_start`,
+  `date_end`,
+  `user_id`,
+  `category_id`,
+  `is_suspend`,
+  `is_publish`
+) VALUES (
+  7,
+  'Tournevis Etoile',
+  'Un tournevis parfait pour demonter vos macs et changer vos disque dur sans encore avoir a donner des milliers deuros a Apple !',
+  NULL,
+  'tournevis.png',
+  now(),
+  '2016-01-03',
+  '2',
+  '4',
+  0,
+  1
+);
+
+INSERT INTO `advert` (
+  `id`,
+  `title`,
+  `description`,
+  `short_description`,
+  `image`,
+  `date_start`,
+  `date_end`,
+  `user_id`,
+  `category_id`,
+  `is_suspend`,
+  `is_publish`
+) VALUES (
+  8,
+  'Réseaux - Tanenbaum',
+  'Je met a votre disposition le livre que personne n a le temps ni l envie de lire ! Il vous permettra d avoir de superbe note dans votre matiere preferée... le RESEAUX biensur !',
+  NULL,
+  'livrereseau.png',
+  now(),
+  '2016-01-03',
+  '2',
+  '6',
+  0,
+  1
+);
