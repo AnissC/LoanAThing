fcbcf473b3027c3a810d319dd34a90db6cdd84c2
Nico Plop <nyckoo@live.fr>
2015-12-28 23:12:09 +0100

    [backoffice-home] fixed

--- src/com/lat/servlets/BoHome.java
+++ src/com/lat/servlets/BoHome.java
@@ -6,17 +6,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-/**
- * Created by Nico on 28/12/2015.
- */
+
 
 @WebServlet("/backoffice")
-public class BoHome extends HttpServlet{
-
-    public void init() throws ServletException
-    {
-    }
-
+public class BoHome extends HttpServlet
+{
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
         this.getServletContext().getRequestDispatcher("/WEB-INF/views/backoffice/home.jsp").forward(request, response);
