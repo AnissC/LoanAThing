e88fee9e85bb9c7df32c56c1440bbcbdf07b1f2b
Nico Plop <nyckoo@live.fr>
2015-12-29 12:14:03 +0100

    [admin-access] Admin filter for BackOffice

--- src/com/lat/servlets/BoUserReport.java
+++ src/com/lat/servlets/BoUserReport.java
@@ -1,7 +1,5 @@
 package com.lat.servlets;
 
-import com.lat.services.UserService;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,25 +10,9 @@ import java.io.IOException;
 @WebServlet("/backoffice/report/users")
 public class BoUserReport extends HttpServlet
 {
-    private UserService userService;
-
-    public void init() throws ServletException
-    {
-        this.userService = UserService.getInstance();
-    }
-
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
-        String UserInSessionGroup = userService.getUserInSession().getGroup().getName();
-
-        if(UserInSessionGroup != null && UserInSessionGroup.equals("ADMIN"))
-        {
             this.getServletContext().getRequestDispatcher("/WEB-INF/views/backoffice/userReport.jsp").forward(request, response);
-        }
-        else
-        {
-            response.sendRedirect("/home");
-        }
     }
 }
 
